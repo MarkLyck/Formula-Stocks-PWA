8db5b9e7e3bd724ec55ce5e59f3afe5ffc6da11e
MarkLyck <mark.lyck@williamhill.com>
2017-09-21 15:35:10 +0200

    prefetching Dashboard links

--- pages/dashboard/SideMenu/MenuItem/MenuItem.js
+++ pages/dashboard/SideMenu/MenuItem/MenuItem.js
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types'
 import Router from 'next/router'
 import { Button } from './styles'
 
-const MenuItem = ({ route, children }) => (
-    <Button onClick={() => Router.push(`/dashboard/${route}`)}>
-        {children}
-    </Button>
-)
+const MenuItem = ({ route, children }) => {
+    if (typeof window !== 'undefined' && Router.router && route) {
+        Router.prefetch(`/dashboard/${route}`)
+    }
+
+    return (
+        <Button onClick={() => Router.push(`/dashboard/${route}`)}>
+            {children}
+        </Button>
+    )
+}
 
 MenuItem.propTypes = {
     route: PropTypes.string,
