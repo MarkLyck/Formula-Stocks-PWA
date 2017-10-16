a470d5ed4354f00f746e479f199e988ca919e74d
MarkLyck <mark.lyck@williamhill.com>
2017-10-16 11:05:30 +0200

    statistics Container responsive

--- components/statisticsContainer/styles.js
+++ components/statisticsContainer/styles.js
@@ -2,9 +2,14 @@ import styled from 'emotion/react'
 
 export const Statistics = styled('div')`
     display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
     align-items: center;
     justify-content: space-between;
     padding: 8px;
+    > div {
+        width: calc(100% / 4 - 16px)
+    }
     > div:first-child {
         background: ${props => props.theme.colors.primary};
         color: ${props => props.theme.colors.white};
@@ -25,6 +30,26 @@ export const Statistics = styled('div')`
         color: ${props => props.theme.colors.secondary};
         > div:last-child {
             border-left: 1px solid #eaeaeb;
+        }
+    }
+    @media (max-width: 1200px) {
+        > div {
+            width: calc(100% / 2 - 16px);
+        }
+        > div:first-child {
+            margin-bottom: 16px;
+        }
+        > div:nth-child(2) {
+            margin-bottom: 16px;
+        }
+    }
+    @media (max-width: 600px) {
+        > div {
+            width: calc(100% - 16px);
+            margin-bottom: 16px;
+        }
+        > div:last-child {
+            margin-bottom: 0;
         }
     }
 `
