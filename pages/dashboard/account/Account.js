6809dc4d309b008693ebf9e696a77340d436b525
MarkLyck <mark.lyck@williamhill.com>
2017-10-25 15:34:30 +0200

    Account page

--- pages/dashboard/account/Account.js
+++ pages/dashboard/account/Account.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { gql, graphql } from 'react-apollo'
+import Paper from 'material-ui/Paper'
+import Button from 'material-ui/Button'
 import Dashboard from '../'
+import { MyAccountContainer } from './styles'
 
 class Account extends Component {
     render() {
@@ -9,7 +12,20 @@ class Account extends Component {
         console.log(User)
         return (
             <Dashboard>
-                test
+                <MyAccountContainer>
+                    <h4>My Account</h4>
+                    <Paper style={{ padding: '16px' }}>
+                        <h4 className="user-info">{User.name}</h4>
+                        <h4 className="user-info">{User.email}</h4>
+                        <h4 className="user-info user-plan">{User.plan} Model</h4>
+                    </Paper>
+                </MyAccountContainer>
+                <Button
+                    raised
+                    style={{ margin: '0 auto', display: 'block' }}
+                >
+                    Cancel subscription
+                </Button>
             </Dashboard>
         )
     }
