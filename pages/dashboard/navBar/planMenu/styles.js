5e0d3228302430d023e7b90115d896a9d823fdf8
MarkLyck <mark.lyck@williamhill.com>
2017-10-31 16:14:53 +0100

    remove unneeded styles

--- pages/dashboard/navBar/planMenu/styles.js
+++ pages/dashboard/navBar/planMenu/styles.js
@@ -1,11 +1,5 @@
 import styled from 'emotion/react'
 
-export const MenuContainer = styled('span')`
-    display: none;
-    @media(max-width: 600px) {
-        display: block;
-    }
-`
 export const SelectedPlanName = styled('p')`
     margin-right: 8px;
 `
